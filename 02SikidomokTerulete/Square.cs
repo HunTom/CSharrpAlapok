f260124edf8cf31e77b5b33db57cc00a9ff2bd30
Tamas Simon <tom_76@t-online.hu>
2018-09-02 19:33:10 +0200

    #1 SikidomokTeruleteCreate

---
+++ 02SikidomokTerulete/Square.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02SikidomokTerulete
+{
+    public class Square
+    {
+        private int side;
+
+        public Square(int side)
+        {
+            this.side = side;
+        }
+
+        public int Area()
+        {
+            //a this.side nem kell lehet helyett side, mert itt nem zavar be a paraméter ugyanazon neve
+            return side * side;
+        }
+    }
+}
